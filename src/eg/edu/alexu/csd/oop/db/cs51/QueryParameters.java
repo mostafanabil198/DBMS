7c9f44caf3c2344ec89d9d15d3bc9b4f677904be
muhammad salah <msalah.29.10@gmail.com>
2018-11-25 20:14:58 +0200

    added queryParameters

--- src/eg/edu/alexu/csd/oop/db/cs51/QueryParameters.java
+++ src/eg/edu/alexu/csd/oop/db/cs51/QueryParameters.java
@@ -1,5 +1,102 @@
 package eg.edu.alexu.csd.oop.db.cs51;
 
-public class QueryParameters {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import javafx.util.Pair;
+
+public class QueryParameters {
+    private static final String DB_NAME = "DB_NAME";
+    private static final String TABLE_NAME = "TABEL_NAME";
+    private static final String COL_NAME = "COL";
+    private static final String COL_TYPE = "TYPE";
+    private static final String COL_VALUE = "VALUE";
+    private static final String CONDITION = "CONDITION";
+    
+    private Map<String, String> param;
+    private int colNum;
+    
+    public QueryParameters() {
+        param = new HashMap<String , String>();
+        colNum = 0;
+    }
+    /****************** setters*******************/
+    
+    public void setDatabaseName(String DBName) {
+        param.put(this.DB_NAME, DBName);
+    }
+    
+    public void setTableName(String tableName) {
+        param.put(this.TABLE_NAME, tableName);
+    }
+    
+    public void addColumnType(String colName, String colType) {
+        colNum++;
+        param.put(this.COL_NAME + colNum, colName);
+        param.put(this.COL_TYPE + colNum, colType);
+    }
+    
+    public void addColumnValue(String columnName, String value) {
+        colNum++;
+        param.put(this.COL_NAME + colNum, columnName);
+        param.put(this.COL_VALUE + colNum, value);
+    }
+    
+    public void addColumnValue(String value) {
+        colNum++;
+        param.put(this.COL_NAME + colNum, String.valueOf(colNum));
+        param.put(this.COL_VALUE + colNum, value);
+    }
+    
+    public void addColumnName(String columnName) {
+        colNum++;
+        param.put(this.COL_NAME + colNum, columnName);
+    }
+    
+    public void setCondition(String condition) {
+        param.put(this.CONDITION, condition);
+    }
+    
+    /***************getters***************/
+    
+    public List<Pair<String, String>> getColumnsType(){
+        List<Pair<String, String>> columns = new ArrayList<Pair<String, String>>();
+        for(int i = 1; i <= colNum; i++) {
+            columns.add(new Pair(param.get(this.COL_NAME + i), param.get(this.COL_TYPE + i)));
+        }
+        
+        return columns;
+    }
+    
+    public List<Pair<String, String>> getColumnsValue(){
+        List<Pair<String, String>> columns = new ArrayList<Pair<String, String>>();
+        for(int i = 1; i <= colNum; i++) {
+            columns.add(new Pair(param.get(COL_NAME + i), param.get(COL_VALUE + i)));
+        }
+        
+        return columns;
+    }
+    
+    public List<String> getColumnsName(){
+        List<String> columns = new ArrayList<String>();
+        for(int i = 1; i <= colNum; i++) {
+            columns.add(param.get(COL_NAME + i));
+        }
+        
+        return columns;
+    }
+    
+    public String getCondition() {
+        return param.get(this.CONDITION);
+    }
+    
+    public String getDatabaseName() {
+        return param.get(this.DB_NAME);
+    }
+    
+    public String getTableName() {
+        return param.get(this.TABLE_NAME);
+    }
 }
