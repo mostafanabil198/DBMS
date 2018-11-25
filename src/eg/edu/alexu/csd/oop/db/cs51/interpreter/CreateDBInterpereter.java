7577bd3d9e73c5294370b372a684b8e80f5afa88
mostafa nabil <mostafanabil198@gmail.com>
2018-11-25 22:18:48 +0200

    added create database interpreter

---
+++ src/eg/edu/alexu/csd/oop/db/cs51/interpreter/CreateDBInterpereter.java
@@ -0,0 +1,25 @@
+package eg.edu.alexu.csd.oop.db.cs51.interpreter;
+
+import java.sql.SQLException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import eg.edu.alexu.csd.oop.db.cs51.QueryParameters;
+
+public class CreateDBInterpereter implements Interpreter {
+	private static final String CREATE_DB_PATTERN = "create database ([A-Za-z_][A-Za-z0-9_]*)";
+
+	@Override
+	public QueryParameters interpret(String query) {
+		Pattern pattern = Pattern.compile(CREATE_DB_PATTERN, Pattern.CASE_INSENSITIVE);
+		Matcher matcher = pattern.matcher(query);
+		if (matcher.matches()) {
+			QueryParameters queryParameters = new QueryParameters();
+			queryParameters.setDatabaseName(matcher.group(1));
+			return queryParameters;
+		} else {
+			throw new SQLException();
+		}
+	}
+
+}
