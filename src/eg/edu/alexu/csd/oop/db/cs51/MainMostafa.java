79f062f377347e46bbbefbbc0384c122263f746e
mostafa nabil <mostafanabil198@gmail.com>
2018-11-26 22:16:48 +0200

    testing save table

--- src/eg/edu/alexu/csd/oop/db/cs51/MainMostafa.java
+++ src/eg/edu/alexu/csd/oop/db/cs51/MainMostafa.java
@@ -1,6 +1,7 @@
 package eg.edu.alexu.csd.oop.db.cs51;
 
 import java.io.File;
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -9,6 +10,11 @@ import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
+
+import eg.edu.alexu.csd.oop.db.cs51.filesParsers.TableLoader;
 import eg.edu.alexu.csd.oop.db.cs51.filesParsers.TableWriter;
 import eg.edu.alexu.csd.oop.db.cs51.interpreter.CreateTableInterpreter;
 import eg.edu.alexu.csd.oop.db.cs51.interpreter.DropTableInterpreter;
@@ -19,38 +25,48 @@ import eg.edu.alexu.csd.oop.db.cs51.utilities.Pair;
 
 public class MainMostafa {
 
-	public static void main(String[] args) throws SQLException {
-		List<Map<String,String>> list = new ArrayList<>();
-//		Map m = new HashMap<>();
-//		m.put("id", "1");
-//		m.put("name", "mostafa");
-//		m.put("age", "20");
-//		m.put("gender", "male");
-//		list.add(m);
-//		
-//		m = new HashMap<>();
-//		m.put("id", "2");
-//		m.put("name", "nashar");
-//		m.put("age", "20");
-//		m.put("gender", "male");
-//		list.add(m);
-//		
-//		m = new HashMap<>();
-//		m.put("id", "3");
-//		m.put("name", "tarek");
-//		m.put("age", "21");
-//		m.put("gender", "male");
-//		list.add(m);
-//		
-//		m = new HashMap<>();
-//		m.put("id", "4");
-//		m.put("name", "sala7");
-//		m.put("age", "21");
-//		m.put("gender", "male");
-//		list.add(m);
+	public static void main(String[] args) throws SQLException, ParserConfigurationException, SAXException, IOException {
+/*		List<Map<String,String>> list = new ArrayList<>();
+		Map m = new HashMap<>();
+		m.put("id", "1");
+		m.put("name", "mostafa");
+		m.put("age", "20");
+		m.put("gender", "male");
+		list.add(m);
+		
+		m = new HashMap<>();
+		m.put("id", "2");
+		m.put("name", "nashar");
+		m.put("age", "20");
+		m.put("gender", "male");
+		list.add(m);
+		
+		m = new HashMap<>();
+		m.put("id", "3");
+		m.put("name", "tarek");
+		m.put("age", "21");
+		m.put("gender", "male");
+		list.add(m);
+		
+		m = new HashMap<>();
+		m.put("id", "4");
+		m.put("name", "sala7");
+		m.put("age", "21");
+		m.put("gender", "male");
+		list.add(m);
 		
 		TableWriter tw = new TableWriter();
-		tw.saveTable(new File("saveMostafa.xml"), list);
+		tw.saveTable(new File("saveMostafa.xml"), list);*/
+		
+		TableLoader tl = new TableLoader();
+		List<Map<String,String>> l = tl.load(new File("saveMostafa.xml"));
+		for(Map<String, String> row : l ) {
+//			System.out.println("Row");
+//			for(String s : row.keySet()) {
+//				System.out.println(s + " : " + row.get(s));
+//			}
+			System.out.println(row.size());
+		}
 	}
 
 }
