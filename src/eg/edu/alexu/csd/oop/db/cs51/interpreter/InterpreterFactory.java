913948f69680ee48af1008bb195226640198e699
alnashar <alnasharm778@gmail.com>
2018-11-26 14:12:24 +0200

    edit InterpreterFactory

--- src/eg/edu/alexu/csd/oop/db/cs51/interpreter/InterpreterFactory.java
+++ src/eg/edu/alexu/csd/oop/db/cs51/interpreter/InterpreterFactory.java
@@ -1,6 +1,8 @@
 package eg.edu.alexu.csd.oop.db.cs51.interpreter;
 
 public class InterpreterFactory {
+	private static final String REGEX = "((CREATE +TABLE)|(DROP +TABLE)|(CREATE +DATABASE)|(DROP +DATABASE)|(UPDATE)|(DELETE +FROM)|(SELECT)|(INSERT +INTO)).*";
+	private s
     
     public Interpreter getInterpreterFromQuery(String Query) {
         
