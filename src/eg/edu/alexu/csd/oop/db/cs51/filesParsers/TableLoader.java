ac26b63632e636dc70c164d5695fb68781593a2d
HP <tarek.ahmed2009@yahoo.com>
2018-11-26 21:57:08 +0200

    tableloader

---
+++ src/eg/edu/alexu/csd/oop/db/cs51/filesParsers/TableLoader.java
@@ -0,0 +1,49 @@
+package eg.edu.alexu.csd.oop.db.cs51.filesParsers;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+
+import java.io.File;
+
+import org.xml.sax.SAXException;
+
+public class TableLoader {
+
+	public List<Map<String, String>> load(File path) throws ParserConfigurationException, SAXException, IOException {
+		List<Map<String, String>> rowsValues = new ArrayList<Map<String, String>>();
+		File fXmlFile = path;
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+		Document doc = dBuilder.parse(fXmlFile);
+		doc.getDocumentElement().normalize();
+		NodeList nList = doc.getElementsByTagName("Row");
+		for (int temp = 0; temp < nList.getLength(); temp++) {
+			Node nNode = nList.item(temp);
+			Map<String, String> colValue = new HashMap<String, String>();
+			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+				Element eElement = (Element) nNode;
+				NamedNodeMap rowsContent = eElement.getAttributes();
+				for (int i = 0; i < rowsContent.getLength(); i++) {
+					String key = rowsContent.item(i).getNodeName();
+					String value = rowsContent.item(i).getNodeValue();
+					colValue.put(key, value);
+				}
+				rowsValues.add(colValue);
+			}
+		}
+		return rowsValues;
+	}
+}
