ca18ca4a81eeee0fe0f05be0f30b90b41308aaa4
alnashar <alnasharm778@gmail.com>
2018-11-26 20:05:34 +0200

    edit schema javaa

--- src/eg/edu/alexu/csd/oop/db/cs51/database/Schema.java
+++ src/eg/edu/alexu/csd/oop/db/cs51/database/Schema.java
@@ -8,27 +8,66 @@ public class Schema {
     private List<Pair<String, String>> columnType;
     private String schemaFilePath;
     
-    public Schema() {
+    private Schema() {
         
     }
     
     public Schema(String tableName) {
+    	//get path of database
         //load schema
     }
 
     
     public boolean validateColumnNames(List<String> columns) {
+    	boolean validate;
+    	for(int i = 0;i < columns.size(); i++){
+    		validate = false;
+    		for(int j = 0; j < columnType.size(); j++) {
+    			if(columns.get(i).equals(columnType.get(j).getKey())) {
+    				validate = true;
+    				break;
+    			}
+    		}
+    		if(!validate) {
+    			return false;
+    		}
+    	}
         return true;
     }
     
     public boolean validateColumnValueType(List<Pair<String, String>> columnValue) {
+    	boolean validate;
+    	for(int i = 0 ;i< columnValue.size(); i++) {
+    		validate = false;
+    		for(int j = 0;j< columnType.size();j++) {
+    			if(columnValue.get(i).getKey() == columnType.get(j).getKey()) {
+    				validate = true;
+    				if(columnType.get(j).getValue().equals("int")) {
+    					try {
+							int num = Integer.parseInt(columnValue.get(i).getValue());
+						} catch (Exception e) {
+							// TODO: handle exception
+							return false;
+						}
+    				} 
+    				break;
+    			} 
+    			
+    		} 
+    		if(!validate) {
+    			return false;
+    		}
+    	}
         return true;
     }
     public void save() {
-        
+      
     }
     public static Schema createNewSchema(String tableName, List<Pair<String, String>> columnType) {
-        return new Schema();
+    	Schema schema = new Schema();
+    	schema.columnType = columnType;
+    	//schema.schemaFilePath = database path + tableName + ".dtd";
+        return schema;
     }
 
 }
