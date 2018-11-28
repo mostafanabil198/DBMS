3bcae1402b29f8517fd5e4d325d317a036ee034d
muhammad salah <msalah.29.10@gmail.com>
2018-11-28 12:35:11 +0200

    string to varchar

--- src/eg/edu/alexu/csd/oop/db/cs51/commands/CreateTableCommand.java
+++ src/eg/edu/alexu/csd/oop/db/cs51/commands/CreateTableCommand.java
@@ -1,12 +1,17 @@
 package eg.edu.alexu.csd.oop.db.cs51.commands;
 
+import java.util.List;
+
 import eg.edu.alexu.csd.oop.db.cs51.QueryParameters;
+import eg.edu.alexu.csd.oop.db.cs51.utilities.Pair;
 
 public class CreateTableCommand implements Command {
 
     @Override
     public Object execute(QueryParameters qp) {
-        // TODO Auto-generated method stub
+        String tableName = qp.getTableName();
+        List<Pair<String, String>> columnType = qp.getColumnsType();
+        
         return null;
     }
     
